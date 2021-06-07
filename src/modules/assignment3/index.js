4801e8402ce3e84286d28f739712ac1305463ce8
iamraiaditya45 <aditya.rai@appinventiv.com>
2021-06-07 22:23:36 +0530

    final commit xcoin

--- src/modules/assignment3/index.js
+++ src/modules/assignment3/index.js
@@ -2,18 +2,28 @@ import React,{Component} from 'react';
 import './index.css';
 
 import image from './logo.png';
-import image1 from './noun_Wallet_.png';
 import image8 from './man.png';
 import mastercard from './Mastercard.png';
 import visacard from './visa.png';
 import message from './message.png';
 import noun from './noun.png';
 import you from './you.png';
+import usa from './usa.png';
+import bitcoin from './bitcoin.png';
 import nountick from './noun-tick.png';
+ import nountick1 from './noun_tick_.png';
+ import nountimer from './noun-timer.png';
+ import greentick from './tick-green.png';
+ import greenwallet from './wallet-green.png';
+ import greentimer from './timer-green.png';
+import wallet from './wallet.png';
 import circle1 from './circle1.png';
 import circle2 from './circle2.png';
 import circle3 from './circle3.jpeg';
 import girl from './girl.png';
+import twitter from './twitter.svg';
+import facebook from './facebook.svg';
+import review from './review-footer.png';
 class Home extends Component{
   // renderBox1(img, heading, content,number,extraclass="") {
   //   return (
@@ -27,13 +37,24 @@ class Home extends Component{
   //     </div>
   //   );
   // }
-
-renderBox1(img, heading, content,number,extraclass="") {
+  renderBox1(img, heading, content,number,extraclass="") {
     return (
       <div className={"cardBlue"}>
         <img src={img} className={extraclass}></img>
         <div className={"cardBlueHead"}>
           <div class="oval-active"><span class="oval_inner-active">{number}</span></div>
+          <h3 className={"blueContainerHead"}>{heading}</h3>
+          </div>
+          <p className={"blueContainerPara"}>{content}</p>
+      </div>
+    );
+  }
+  renderBox2(img, heading, content,number,extraclass="") {
+    return (
+      <div className={"cardBlue"}>
+        <img src={img} className={extraclass}></img>
+        <div className={"cardBlueHead"}>
+          <div class="oval-active-green"><span class="oval_inner-active">{number}</span></div>
           <h3 className={"blueContainerHead"}>{heading}</h3>
           </div>
           <p className={"blueContainerPara"}>{content}</p>
@@ -76,8 +97,22 @@ render(){
 <button className={"button4"}>{"BUY"}</button>
     <button className={" button5"}>{"SELL"}</button>
 <div className={"add-money"}>
-<input type="num"  placeholder="100"></input>
+  {/* <img src={usa}></img> */}
+<input type="num"   placeholder="100"></input>
 <div class="circle1"> </div>
+<select className={"currency"}>
+                    <option className={"currency-content"}>USD</option>
+                    <option className={"currency-content"}>INR</option>
+                </select>
+</div>
+<hr className={"hr"}></hr>
+<div className={"add-money"}>
+<input type="num"   placeholder="0.0000965"></input>
+<div class="circle1"> </div>
+<select className={"currency"}>
+                    <option className={"currency-content"}>BTC</option>
+                    <option className={"currency-content"}>DOGE</option>
+                </select>
 </div>
     <button className={" button6"}>{"BUY BITCOIN"}</button>
 </div>
@@ -109,8 +144,38 @@ render(){
 
 </div>
 </div>
-
-
+<div className={"section3"}>
+  <h2 className={"section3Head1"}>{"How to Buy Dogecoin"}</h2>
+  <p className={"section3Head2"}>{"Only 3 simple steps"}</p>
+  <div className={"section3Cards"}>
+  {this.renderBox1(nountick1, "Get verified quickly",
+  "Just sign up, upload your verification documents and open your account.",1
+)}
+{this.renderBox1(wallet, "Buy Dogecoin",
+  "Select which coin you would like to buy and how much you want to puchase.",2
+)}
+{this.renderBox1(nountimer, "Receive coins",
+  "Will send your coins to your wallet within 15 minutes of payment approval.",3,"card3image"
+)}
+  </div>
+  <button type={"button"} className={"section3btn"}>{"BUY DOGECOINS"}</button>
+</div>
+<div className={"section3"}>
+  <h2 className={"section3Head1"}>{"How to Sell Dogecoin"}</h2>
+  <p className={"section3Head2"}>{"Only 3 simple steps"}</p>
+  <div className={"section3Cards"}>
+  {this.renderBox2(greentick, "Get verified quickly",
+  "Just sign up, upload your verification documents and open your account.",1
+)}
+{this.renderBox2(greenwallet, "Sell Dogecoin",
+  "Select Dogecoin under the sell tab and decide how much you want to sell.",2
+)}
+{this.renderBox2(greentimer, "Receive cash",
+  "Will send your coins to your wallet within 15 minutes of payment approval.",3,
+)}
+  </div>
+  <button type={"button"} className={"section4btn"}>{"SELL BITCOINS"}</button>
+</div>
 <div className={"why-to-buy"}>
   <div className={"why-to-buy-image"}>
   <img src={image8} />
@@ -176,6 +241,49 @@ render(){
   </div>
   </div>
  
+
+  <div className={"footer"}>
+                <div className={"footer-img"}>
+                    <img src={image} className={"logo-img"}></img>
+                     <img src={review} className={"review"}></img> 
+                    <p className="footer-address">14 East, Level 8, Sliema Road, GZR1639, Malta</p>
+                </div>
+                <ul className={"company"}>
+                    <h4>COMPANY</h4>
+                    <li>About Us</li>
+                    <li>Careers</li>
+                    <li>Press Kit</li>
+                </ul>
+
+                <ul className={"support"}>
+                    <h4>SUPPORT</h4>
+                    <li>Help Center</li>
+                    <li>Contact</li>
+                </ul>
+
+                <ul className={"social"}>
+                    <h4>SOCIAL</h4>
+                    <li> Instagram</li>
+                    <li>Twitter</li>
+                    <li>Facebook</li>
+                </ul>
+            </div>
+            <div className={"footer-xcoins-term"}>
+                        <span>&copy;XCoins.com</span>
+                        <span>Terms of Service</span>
+                        <span>Privacy Policy</span>
+                        <span>Legal notice</span>
+                        <span>Sitemap</span>
+            
+                <select className={"lang"}>
+                <option className={"lang-content"}></option>
+                    <option className={"lang-content"}>Language</option>
+                    <option className={"lang-content"}>English</option>
+                </select>
+                </div>
+                <div className={"footer-end"}>
+                <p className={"footer-end-content"}>{"Xcoins is property of CF Technologies Ltd - Company #204616970 4 East, Level 8, Sliema Road, GZR1639, Malta. All trademarks and copyrights belong to their respective owners. All rights reserved."}</p>
+                </div>
 </div>
 
         );
