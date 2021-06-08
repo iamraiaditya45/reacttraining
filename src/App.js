d699dc414bd40378b8a50de68d10493a2583378a
iamraiaditya45 <aditya.rai@appinventiv.com>
2021-06-08 17:14:59 +0530

    flex

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 /*import Assignment2 from './modules/assignment2';*/
 // import Assignment3 from './modules/';
- import Assignment4 from './modules/assignment4-grid-mediaquery';
-  // import Assignment4 from './modules/assignment-4-flex';
+//  import Assignment4 from './modules/assignment4-grid-mediaquery';
+   import Assignment4 from './modules/assignment-4-flex';
 class App extends React.Component{
   render(){
     return(
