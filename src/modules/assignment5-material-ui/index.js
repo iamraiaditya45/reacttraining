f3217882f90fe0aaf71313faf63c51ba03d104c7
iamraiaditya45 <aditya.rai@appinventiv.com>
2021-06-10 20:11:26 +0530

    final commit

--- src/modules/assignment5-material-ui/index.js
+++ src/modules/assignment5-material-ui/index.js
@@ -43,6 +43,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableBody from '@material-ui/core/TableBody';
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import AddIcon from '@material-ui/icons/Add';
+import VpnLockIcon from '@material-ui/icons/VpnLock';
 const drawerWidth=78
 const styles=makeStyles((theme)=>({
   
@@ -142,7 +144,8 @@ cardbtn:{
     textAlign:'center'
   },
   switch:{
-    color:"red"
+    color:"green",
+    marginTop:10
   },
   label:{
 color:'black',
@@ -197,8 +200,6 @@ limitcol:{
     fontSize:"20px"
 },
 buybutton:{
-
-    backgroundColor:"orange",
     padding:"7px 25px",
     marginLeft:"-60px",
     fontSize:"15px",
@@ -211,6 +212,145 @@ showbutton:{
     fontSize:"20px",
     marginLeft:"-150px"
 },
+tableheading: {
+  position: "absolute",
+  width: "700px",
+  margin: "490px -20px",
+  color: "grey",
+},
+tablecontainer: {
+  position: "absolute",
+  margin: "570px 110px",
+  width: "1260px",
+  height: "550px",
+  boxShadow: "0 0 2px grey",
+},
+tablehead: {
+  fontSize: "22px",
+  fontWeight: "bold",
+  width: "250px",
+},
+tablehead2: {
+  fontSize: "20px",
+  fontWeight: "bold",
+  width: "350px",
+},
+sellercol: {
+  fontSize: "17px",
+  color: "blue",
+},
+paymentcol: {
+  fontSize: "20px",
+},
+pricecol: {
+  fontSize: "22px",
+  color: "green",
+  fontWeight: "bold",
+},
+limitcol: {
+  fontSize: "20px",
+},
+buybutton: {
+  backgroundColor: "orange",
+  color:"white",
+  padding: "7px 25px",
+  marginLeft: "-60px",
+  fontSize: "15px",
+  fontWeight: "bold",
+},
+showbutton: {
+  width: "200px",
+  fontWeight: "bold",
+  fontSize: "20px",
+  marginLeft: "-150px",
+},
+tablebottom: {
+  position: "absolute",
+  margin: "2340px 110px",
+},
+youcanbe: {
+  fontSize: "25px",
+  fontWeight: "bold",
+},
+postadbutton: {
+  color: "green",
+  fontSize: "20px",
+  border: "1px solid green",
+},
+addicon: {
+  fontSize: "30px",
+  color: "green",
+},
+buymonero: {
+  color: "green",
+  fontSize: "24px",
+},
+showingad: {
+  position: "absolute",
+  margin: "2480px 500px",
+},
+showingcontent: {
+  fontSize: "22px",
+  width: "500px",
+},
+changebutton: {
+  color: "blue",
+  fontSize: "22px",
+},
+horizontal: {
+  width: "1200px",
+  position: "absolute",
+  margin: "2130px 130px",
+},
+bottombody: {
+  position: "absolute",
+  margin: "2570px 300px",
+},
+bottomhead: {
+  fontWeight: "bold",
+  padding: "15px 0px",
+},
+bottomcontent: {
+  fontSize: "18px",
+},
+horizontal: {
+  width: "1200px",
+  position: "absolute",
+  margin: "2535px 130px",
+},
+accounticon: {
+  color: "blue",
+},
+tableheading2: {
+  position: "absolute",
+  width: "700px",
+  margin: "1160px -20px",
+  color: "black",
+},
+tablecontainer2: {
+  position: "absolute",
+  margin: "1240px 110px",
+  width: "1260px",
+  height: "550px",
+  boxShadow: "0 0 2px grey",
+},
+tableheading3: {
+  position: "absolute",
+  width: "700px",
+  margin: "1820px 40px",
+  color: "black",
+},
+tablecontainer3: {
+  position: "absolute",
+  margin: "1880px 110px",
+  width: "1260px",
+  height: "422px",
+  boxShadow: "0 0 2px grey",
+},
+distancecol: {
+  fontSize: "20px",
+},
+
 }));
 
 
@@ -351,7 +491,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell  className={classes.paymentcol}> IMPS Bank Transfer India</TableCell>
                           <TableCell className={classes.pricecol}>19366.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>2500.00-20000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained" className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -359,7 +499,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell className={classes.paymentcol}>Other online payment</TableCell>
                           <TableCell className={classes.pricecol}>19566.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>100.00-22000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained" className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -367,7 +507,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell className={classes.paymentcol}>IMPS Bank Transfer India</TableCell>
                           <TableCell className={classes.pricecol}>21366.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>8500.00-50000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained" className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -375,7 +515,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell className={classes.paymentcol}>IMPS Bank Transfer India</TableCell>
                           <TableCell className={classes.pricecol}>21966.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>1000.00-25000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained" className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -383,7 +523,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell className={classes.paymentcol}>IMPS Bank Transfer India</TableCell>
                           <TableCell className={classes.pricecol}>19366.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>2500.00-20000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained" className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -391,7 +531,7 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell className={classes.paymentcol}>IMPS Bank Transfer India</TableCell>
                           <TableCell className={classes.pricecol}>21766.77 INR</TableCell>
                           <TableCell className={classes.limitcol}>Up to 25000.00 INR</TableCell>
-                          <TableCell><Button variant="contained" color="primary"className={classes.buybutton}>BUY
+                          <TableCell><Button variant="contained"className={classes.buybutton}>BUY
                                   </Button></TableCell>
                       </TableRow>
                       <TableRow>
@@ -400,13 +540,234 @@ classes={{paper:classes.drawerPaper}}>
                           <TableCell></TableCell>
                           <TableCell></TableCell>
                           <TableCell><Button color="primary" className={classes.showbutton}>SHOW MORE...</Button></TableCell>
-                      </TableRow>
-                      
+                      </TableRow> 
                   </TableBody>
               </Table>
           </TableContainer>
           </div>
-          </div>
+          <div className={classes.tableheading3}>
+          <Typography variant="h3" align="right">
+            Sell Monero For Cash in India
+          </Typography>
+        </div>
+        <div className={classes.tablecontainer3}>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow hover>
+                  <TableCell className={classes.tablehead}>Buyer</TableCell>
+                  <TableCell className={classes.tablehead}>Distance</TableCell>
+                  <TableCell className={classes.tablehead2}>Location</TableCell>
+                  <TableCell className={classes.tablehead}>Price/XMR</TableCell>
+                  <TableCell className={classes.tablehead2}>Limits</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow hover>
+                  <TableCell className={classes.sellercol}>
+                    <u>chandanbits007(54;100%)</u>
+                  </TableCell>
+                  <TableCell className={classes.distancecol}>
+                    <u>500.0KM</u>
+                  </TableCell>
+                  <TableCell className={classes.paymentcol}>
+                    <LocationOnIcon
+                      fontSize={"small"}
+                      className={classes.accounticon}
+                    />
+                    DELHI
+                  </TableCell>
+                  <TableCell className={classes.pricecol}>
+                    19366.77 INR
+                  </TableCell>
+                  <TableCell className={classes.limitcol}>
+                    250.00-20000.00 INR
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      className={classes.buybutton}
+                    >
+                      SELL
+                    </Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow hover>
+                  <TableCell className={classes.sellercol}>
+                    <u>appukttam0(529;100%)</u>
+                  </TableCell>
+                  <TableCell className={classes.distancecol}>
+                    <u>450.0KM</u>
+                  </TableCell>
+                  <TableCell className={classes.paymentcol}>
+                    <LocationOnIcon
+                      fontSize={"small"}
+                      className={classes.accounticon}
+                    />
+                    MUMBAI
+                  </TableCell>
+                  <TableCell className={classes.pricecol}>
+                    19566.77 INR
+                  </TableCell>
+                  <TableCell className={classes.limitcol}>
+                    100.00-22000.00INR
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      className={classes.buybutton}
+                    >
+                      SELL
+                    </Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow hover>
+                  <TableCell className={classes.sellercol}>
+                    <u>jdboi(5114;100%)</u>
+                  </TableCell>
+                  <TableCell className={classes.distancecol}>
+                    <u>650.5KM</u>
+                  </TableCell>
+                  <TableCell className={classes.paymentcol}>
+                    <LocationOnIcon
+                      fontSize={"small"}
+                      className={classes.accounticon}
+                    />
+                    LUCKNOW
+                  </TableCell>
+                  <TableCell className={classes.pricecol}>
+                    21366.77 INR
+                  </TableCell>
+                  <TableCell className={classes.limitcol}>
+                    800.00-50000.00INR
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      className={classes.buybutton}
+                    >
+                      SELL
+                    </Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow hover>
+                  <TableCell className={classes.sellercol}>
+                    <u>chgbyry(4535;100%)</u>
+                  </TableCell>
+                  <TableCell className={classes.distancecol}>
+                    <u>530.0KM</u>
+                  </TableCell>
+                  <TableCell className={classes.paymentcol}>
+                    <LocationOnIcon
+                      fontSize={"small"}
+                      className={classes.accounticon}
+                    />
+                    JAIPUR
+                  </TableCell>
+                  <TableCell className={classes.pricecol}>
+                    21966.77 INR
+                  </TableCell>
+                  <TableCell className={classes.limitcol}>
+                    900.00-25000.00INR
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      className={classes.buybutton}
+                    >
+                      SELL
+                    </Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <Button color="primary" className={classes.showbutton}>
+                      SHOW MORE...
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+                  </div>
+                  <div className={classes.tablebottom}>
+          <Typography className={classes.youcanbe}>
+            You can be the first one to{" "}
+            <Button
+              variant="outlined"
+              color="primary"
+              className={classes.postadbutton}
+              startIcon={
+                <AddIcon fontSize={"large"} className={classes.addicon} />
+              }
+            >
+              POST AN AD
+            </Button>
+            in the following categories.
+          </Typography>
+          <Typography className={classes.buymonero}>
+            <u>
+              <AddIcon fontSize={"large"} className={classes.addicon} />
+              Buy Monero with cash in India
+            </u>
+          </Typography>
+        </div>
+        <div className={classes.showingad}>
+          <Typography className={classes.showingcontent}>
+            <VpnLockIcon fontSize={"large"} />
+            Show advertisements in India
+            <Button
+              variant="container"
+              color="primary"
+              className={classes.changebutton}
+            >
+              CHANGE
+            </Button>
+          </Typography>
+        </div>
+        <div className={classes.horizontal}>
+          <hr></hr>
+        </div>
+        <div className={classes.bottombody}>
+          <Typography variant="h5" className={classes.bottomhead}>
+            Protect your right to privacy - buy Monero.
+          </Typography>
+          <Typography className={classes.bottomcontent}>
+            If you are concerned about the invasion of privacy - the best
+            cryptocurrency to invest in is XMR. Monero is an untraceable coin
+            developed with privacy by design in mind.
+          </Typography>
+          <Typography className={classes.bottomcontent}>
+            Where to buy Monero? LocalMonero is the biggest, most trusted and
+            well-established P2P Monero exchange in the XMR community. We have
+            no KYC checks - you can simply buy Monero anonymously without ID
+            verification with PayPal, credit card, gift card, cash by mail or
+            convert bitcoin to Monero - our platform supports any payment
+            method.
+          </Typography>
+          <Typography variant="h5" className={classes.bottomhead}>
+            Sell Monero to people worldwide or locally - support the community
+            and make money with cryptocurrency.
+          </Typography>
+          <Typography className={classes.bottomcontent}>
+            LocalMonero is the best place to sell XMR - whether it's your mining
+            rewards, Monero price arbitrage, or you simply see the value in
+            servicing the community. The escrow system and our secure Monero
+            wallet provide you with a robust XMR trading experience - so you can
+            convert XMR to USD, EUR, AUD, GBP or any other local currency with
+            peace of mind.
+          </Typography>
+        </div>
+        <div className={classes.horizontal2}>
+          <hr></hr>
+        </div>
 </div>
   );
 }
